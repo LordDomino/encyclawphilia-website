afb0627c0ade784af013567cf6f120d9c44dca77
Loui Dominic Naquita <louidominicnaquita16@gmail.com>
2026-05-18 00:09:40 +0800

    Modified connection script

--- config/database.php
+++ config/database.php
@@ -2,7 +2,7 @@
 // database.php
 function getDatabaseConnection() {
     try {
-        return new PDO('mysql:host=sql209.infinityfree.com;dbname=if0_41928864_encyclawphilia_db', '6WyumXiRAWG0', '');
+        return new PDO('mysql:host=sql209.infinityfree.com;dbname=if0_41928864_encyclawphilia_db', 'if0_41928864', '6WyumXiRAWG0');
     } catch (PDOException $e) {
         die("Database connection failed: " . $e->getMessage());
     }
