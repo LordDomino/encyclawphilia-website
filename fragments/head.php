31c8f37964851ef1c11b40defd11356262089af4
Loui Dominic Naquita <louidominicnaquita16@gmail.com>
2026-05-17 17:18:21 +0800

    Latest UI changes on head.php

--- fragments/head.php
+++ fragments/head.php
@@ -5,5 +5,5 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $pageTitle; ?></title>
-    <link rel="stylesheet" href="css/style-claude.css">
+    <link rel="stylesheet" href="css/style.css">
 </head>
