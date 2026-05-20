16e99610c16202509a946d4106c6d61d47bca2f5
Loui Dominic Naquita <louidominicnaquita16@gmail.com>
2026-05-20 20:29:27 +0800

    Added Google Search Console tag verification

--- src/Views/head.php
+++ src/Views/head.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="google-site-verification" content="Aj0rwC2OO3km5QpsVYiFonJkqjfOrn7ZIgnHu_O5oew" />
     <title><?php echo $pageTitle; ?></title>
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/add_ordinance.css">
